17d5d8ec2febfef1c89ddc081291885074c534d4
karthikeyan rajendran <karthik_ricssion@yahoo.com>
2020-06-09 14:31:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './normalize.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="cflex full-height">
+      <div className="cflex full-height form-elements">
+        <ul>
+          <li>Header</li>
+          <li>Text Input</li>
+          <li>Button</li>
+        </ul>
+      </div>
+      <div className="cflex full-height form-builder">PlayZone</div>
+    </div>
+  );
+}
+
+export default App;
